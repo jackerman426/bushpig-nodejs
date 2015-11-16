b0e7f526fa0e378b7c7a231e4b7eb7febf919279
mtzakris <mtzakris@yahoo.gr>
2015-11-16 09:23:47 +0100

    structure_login

--- service.js
+++ service.js
@@ -1,14 +1,32 @@
-require('./lib').init(function(error, service){
-    if(error){
-        betLogger.error("FATAL ERROR: Core modules are not loaded")
-    }else{
+/**
+ * Created by jackerman on 14/06/15.
+ */
+'use strict';
+
+var async = require('async');
+
+
+async.waterfall([
+    function (callback) {
+        require('./lib').init(function(error, service){
+            if(error){
+                betLogger.error("FATAL ERROR: Core modules are not loaded")
+                callback(true);
+            }else{
+                callback(null, service);
+            }
+        })
+    },
+    function(service, callback) {
         service.establishConnections(function(error){
             if(error){
                 betLogger.error("FATAL ERROR: failed to connect to required resources");
+                callback(true);
             }else{
-                service.start();
+                callback(null, service);
             }
         })
-
     }
+],function(error, service){
+    if(!error) service.initialize();
 });
